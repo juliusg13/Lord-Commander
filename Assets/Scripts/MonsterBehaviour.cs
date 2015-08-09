cf7ec6f746c10db0fe4e077e25d4b11bbf78666d
Júlíus Gíslason <juliusg13@ru.is>
2015-08-09 21:53:29 +0000

    sweet !

---
+++ Assets/Scripts/MonsterBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterBehaviour : MonoBehaviour {
+		
+	public float moveSpeed;
+	public float health;
+	
+	public Quaternion rotation = Quaternion.identity;
+	string direction = "south"; 
+	
+	
+	// Use this for initialization
+	void Start () {
+		//	controller = GameObject.FindGameObjectWithTag ("GameController").GetComponent<Controller>();
+		//currentSpeed = moveSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void FixedUpdate (){
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (transform.localScale.x * 0, transform.localScale.y * -moveSpeed);	
+	}
+	
+	
+}
+	
