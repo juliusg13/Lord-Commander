b2ad952fbfb8272484114c838de797a76a152a8c
Júlíus Gíslason <juliusg13@ru.is>
2015-08-11 23:57:45 +0000

    wat

--- Assets/Scripts/LordAttack2.cs
+++ Assets/Scripts/LordAttack2.cs
@@ -20,10 +20,12 @@ public class LordAttack2 : MonoBehaviour {
 
 	void OnMouseDown() {
 		if (this.gameObject.tag == "Monster") {
+
 			//Rigidbody2D newShot = Instantiate (Projectile, new Vector3 (0.52f, -0.18f, -1f), transform.rotation) as Rigidbody2D;
 			//newShot.AddForce = new Vector2(transform.forward * 200f);
 
 			GameObject projectile = Instantiate( Projectile, new Vector3 (0.52f, -0.18f, -1f), transform.rotation ) as GameObject;
+			//projectile.GetComponent<Projectile2>().myTarget = this.transform;
 			Vector2 force = new Vector2(0f, 3f);
 			projectile.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
 		}
---
+++ Assets/Scripts/Projectile2.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile2 : MonoBehaviour {
+	//public Transform myTarget;
+	//public float speed = 1;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Vector2 targetDir = myTarget.position - transform.position;
+		//float step = speed * Time.deltaTime;
+		//Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+		//Debug.DrawRay(transform.position, newDir, Color.red);
+		//transform.rotation = Quaternion.LookRotation(newDir);
+
+	}
+}
